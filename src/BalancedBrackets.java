d2f24ab8c93b6822ab208c73d763e50848445e05
gr3g <szekelygergoo@gmail.com>
2020-10-21 23:11:10 +0200

    solution for https://www.hackerrank.com/challenges/balanced-brackets/problem

---
+++ src/BalancedBrackets.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class BalancedBrackets {
+
+    static String isBalanced(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '{' || c == '[' || c == '(') {
+                stack.push(c);
+            } else if (c == '}' || c == ']' || c == ')') {
+                if (stack.isEmpty()) {
+                    return "NO";
+                } else {
+                    char lastItem = stack.pop();
+                    if ((lastItem == '{' && c != '}') ||
+                        (lastItem == '[' && c != ']') ||
+                        (lastItem == '(' && c != ')')) {
+                        return "NO";
+                    }
+                }
+            }
+        }
+        return stack.isEmpty() ? "YES" : "NO";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String s = scanner.nextLine();
+
+            String result = isBalanced(s);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
